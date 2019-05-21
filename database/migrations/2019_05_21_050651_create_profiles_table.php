0c42314f8c805349470d800c7b3894853bfb3e80
Tsogtbayar Davaadorj <acep.world@gmail.com>
2019-05-21 13:43:40 +0800

    migrations created

---
+++ database/migrations/2019_05_21_050651_create_profiles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('role_id');
+            $table->string('name', 100);
+            $table->string('nickname', 100);
+            $table->string('email', 250);
+            $table->string('avatar', 250);
+            $table->string('language', 2);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
