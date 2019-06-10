8b3b6ac48ba4f721642322f2fb91aaaf25bc4dce
Tsogtbayar Davaadorj <acep.world@gmail.com>
2019-06-10 13:51:45 +0800

    removed unnessary routes

--- routes/web.php
+++ routes/web.php
@@ -13,25 +13,6 @@
 
 Route::middleware(['auth'])->group(function () {
 
-    Route::get('/test/blank', function () {
-        return view('test.blank');
-    });
-
-    Route::get('/test/example', function () {
-        return view('test.example');
-    });
-
-    Route::get('/test/login', function () {
-        return view('test.login');
-    });
-
-    // Route::resource('menus', 'MenuController');
-
-    // Route::get('/home', 'HomeController@index')->name('home');
-
-    // Route::get('/admin', 'AdminController@index')->name('admin.index');
-
-    // Route::resource('profiles', 'ProfileController');
     Route::get('/admin', 'AdminController@index')->name('admin.index');
     Route::get('/home', 'HomeController@index')->name('home');
     Route::resource('menus', 'MenuController');
