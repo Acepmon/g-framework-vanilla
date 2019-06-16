81cbfadccaa2a4d76c420555d512c2c014943eb9
Tsogtbayar Davaadorj <acep.world@gmail.com>
2019-06-16 18:22:49 +0800

    added statuses to backups and themes

---
+++ app/Backup.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Backup extends Model
+{
+    const IN_PROGRESS = 'in progress';
+    const COMPLETED = 'completed';
+    const FAILED = 'failed';
+    const PREPARING = 'preparing';
+    const RESTORED = 'restored';
+    const DELETED = 'deleted';
+
+    const STATUS_ARRAY = [
+        self::IN_PROGRESS,
+        self::COMPLETED,
+        self::FAILED,
+        self::PREPARING,
+        self::RESTORED,
+        self::DELETED
+    ];
+}
