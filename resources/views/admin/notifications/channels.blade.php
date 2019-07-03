ed228f4a14027cfecfd90b98b2fc7ec9b4a24774
Tsogtbayar Davaadorj <acep.world@gmail.com>
2019-07-03 18:56:05 +0800

    notification management pages added

---
+++ resources/views/admin/notifications/channels.blade.php
@@ -0,0 +1,19 @@
+@extends('themes.limitless.layouts.default')
+
+@section('title', 'Notification Channels')
+
+@section('load')
+
+@endsection
+
+@section('pageheader')
+    @include('admin.notifications.includes.pageheader')
+@endsection
+
+@section('content')
+
+@endsection
+
+@section('script')
+
+@endsection
