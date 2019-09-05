215a423832c83830b18caaeea770c43ad2964e98
tamir <cry4bloodcare@gmail.com>
2019-09-05 17:54:28 +0800

    tamir car home page data

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,11 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Database\Eloquent\Model;
+use App\Content;
+use App\ContentMeta;
+use DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +30,22 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        Blade::directive('contents', function ($expression) {
+            $someObject = json_decode($expression);
+            $contents = Content::whereRaw('1 = 1');
+            foreach ($someObject as $some) {
+                $contents = $contents->where($some->field, '=', $some->key);
+            }
+            $contents = $contents->get();
+            foreach ($contents as $content){
+                $content->metas = $content->metas()->get();
+            }
+            return $contents;
+        });
+
+//        Blade::directive('content', function ($expression) {
+/*            return "<?php echo ($expression)->format('m/d/Y H:i'); ?>";*/
+//        });
     }
 }
--- resources/views/pages/car-web_published_1567573753.blade.php
+++ resources/views/pages/car-web_published_1567573753.blade.php
@@ -157,29 +157,85 @@
                 <div class="row">
                     <div class="card-slide owl-carousel owl-theme">
                         <!-- card start -->
+                        {{--@foreach($menus as $data)--}}
+                            {{--<option value="{{$data->id}}">{{$data->title}}</option>--}}
+                        {{--@endforeach--}}
+
+                        @php
+                        $hotDeal = [1]
+                        @endphp
+                        {{--@define $hotDeal = @contents([{"field":"type", "key":"car"}, {"field":"status", "key":"draft"} ])--}}
+                        @foreach($hotDeal as $data)
+
+                        @endforeach
+
+
                         <div class="card">
                             <div class="brand-name"></div>
                             <div class="card-img">
                                 <img src="{{asset('car-web/img/Cars/1.jpg')}}" class="img-fluid" alt="alt">
                                 <div class="info">
-                  <span class="carIcon-engine">
-                    <p>1499 L</p>
-                  </span>
+                                  <span class="carIcon-engine">
+                                    <p>1499 L</p>
+                                  </span>
+                                                    <span class="carIcon-car-6">
+                                    <p>52,000 KM</p>
+                                  </span>
+                                                    <span class="carIcon-gas-station">
+                                    <p>Gasoline</p>
+                                  </span>
+                                                    <span class="carIcon-gearshift">
+                                    <p>Automatic</p>
+                                  </span>
+                                                    <span class="carIcon-steering-wheel">
+                                    <p>Right wheel</p>
+                                  </span>
+                                                    <span class="color" data-color="white">
+                                    <p>White</p>
+                                  </span>
+                                </div>
+                                <div class="card-caption">
+                                    <div class="meta">2006/2013 | 17,820km | Gasoline <i class="show-more fab fa fa-angle-up"></i></div>
+                                    <div class="favorite">
+                                        <i class="icon-heart"></i>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="card-body shadow">
+                                <div class="card-description">
+                                    <a href="detail-page.html" class="card-title">Santa fe The Prime diesel 2.0 2wd</a>
+                                    <div class="card-meta">
+                                        <div class="status">Change / Finance</div>
+                                        <div class="price">12 Сая ₮</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <div class="brand-name"></div>
+                            <div class="card-img">
+                                <img src="{{asset('car-web/img/Cars/1.jpg')}}" class="img-fluid" alt="alt">
+                                <div class="info">
+                                  <span class="carIcon-engine">
+                                    <p>1499 L</p>
+                                  </span>
                                     <span class="carIcon-car-6">
-                    <p>52,000 KM</p>
-                  </span>
+                                    <p>52,000 KM</p>
+                                  </span>
                                     <span class="carIcon-gas-station">
-                    <p>Gasoline</p>
-                  </span>
+                                    <p>Gasoline</p>
+                                  </span>
                                     <span class="carIcon-gearshift">
-                    <p>Automatic</p>
-                  </span>
+                                    <p>Automatic</p>
+                                  </span>
                                     <span class="carIcon-steering-wheel">
-                    <p>Right wheel</p>
-                  </span>
+                                    <p>Right wheel</p>
+                                  </span>
                                     <span class="color" data-color="white">
-                    <p>White</p>
-                  </span>
+                                    <p>White</p>
+                                  </span>
                                 </div>
                                 <div class="card-caption">
                                     <div class="meta">2006/2013 | 17,820km | Gasoline <i class="show-more fab fa fa-angle-up"></i></div>
@@ -199,8 +255,8 @@
                                 </div>
 
                             </div>
-
                         </div>
+
                         <!-- card end -->
 
                     </div>
