22183c288bbc20ddc9ae602fa2069a2437797a34
Tsogtbayar Davaadorj <acep.world@gmail.com>
2019-09-09 11:48:42 +0800

    banner img mobile w/h updated

--- app/Banner.php
+++ app/Banner.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Banner extends Model
 {
-    //
+    public const MOBILE_WIDTH = 640;
+    public const MOBILE_HEIGHT = 360;
+    public const WEB_WIDTH = 1920;
+    public const WEB_HEIGHT = 1080;
 }
