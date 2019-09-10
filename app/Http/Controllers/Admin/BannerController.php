327b9662f74d1dd60f457bcb14aeae8e16fcc608
Tsogtbayar Davaadorj <acep.world@gmail.com>
2019-09-10 10:19:58 +0800

    banner toggling of active complete

--- app/Http/Controllers/Admin/BannerController.php
+++ app/Http/Controllers/Admin/BannerController.php
@@ -73,38 +73,6 @@ class BannerController extends Controller
             $banner->banner_img_web = url(Storage::url($filename));
         }
 
-        // if ($request->hasFile('banner_img_mobile')) {
-        //     if ($request->has('banner_img_mobile_cropped')) {
-        //         $mobileBase64 = $request->input('banner_img_mobile_cropped');
-        //         $mobileBase64 = str_replace('data:image/png;base64,', '', $mobileBase64);
-        //         $mobileBase64 = str_replace('data:image/jpeg;base64,', '', $mobileBase64);
-        //         $mobileBase64 = str_replace(' ', '+', $mobileBase64);
-        //         $imageName = str_random(10).'.'.'png';
-        //         File::put(storage_path('app'.DIRECTORY_SEPARATOR.'public'.DIRECTORY_SEPARATOR.'banners'). DIRECTORY_SEPARATOR . $imageName, base64_decode($mobileBase64));
-        //         $banner->banner_img_mobile = $imageName;
-        //     } else {
-        //         $mobile_path = $request->banner_img_mobile->store('public/banners');
-        //         $mobile_path = url(Storage::url($mobile_path));
-        //         $banner->banner_img_mobile = $mobile_path;
-        //     }
-        // }
-
-        // if ($request->hasFile('banner_img_web')) {
-        //     if ($request->has('banner_img_web_cropped')) {
-        //         $webBase64 = $request->input('banner_img_web_cropped');
-        //         $webBase64 = str_replace('data:image/png;base64,', '', $webBase64);
-        //         $webBase64 = str_replace('data:image/jpeg;base64,', '', $webBase64);
-        //         $webBase64 = str_replace(' ', '+', $webBase64);
-        //         $imageName = str_random(10).'.'.'png';
-        //         File::put(storage_path('app/public/banners/'). DIRECTORY_SEPARATOR . $imageName, base64_decode($webBase64));
-        //         $banner->banner_img_web = $imageName;
-        //     } else {
-        //         $web_path = $request->banner_img_web->store('public/banners');
-        //         $web_path = url(Storage::url($web_path));
-        //         $banner->banner_img_web = $web_path;
-        //     }
-        // }
-
         $banner->save();
 
         return back()->with('status', 'Banner Successfully created!');
@@ -141,7 +109,48 @@ class BannerController extends Controller
      */
     public function update(Request $request, Banner $banner)
     {
-        //
+        $request->validate([
+            'title' => 'nullable|max:191',
+            'btn_show' => 'nullable|boolean',
+            'btn_text' => 'nullable|max:255',
+            'btn_link' => 'nullable|max:255',
+            'banner_img_mobile' => 'nullable|file',
+            'banner_img_web' => 'nullable|file',
+            'order' => 'nullable|numeric',
+            'active' => 'nullable|boolean'
+        ]);
+
+        if ($request->has('title')) {
+            $banner->title = $request->input('title');
+        }
+
+        if ($request->has('btn_show')) {
+            $banner->btn_show = $request->input('btn_show');
+        }
+
+        if ($request->has('btn_text')) {
+            $banner->btn_text = $request->input('btn_text');
+        }
+
+        if ($request->has('btn_link')) {
+            $banner->btn_link = $request->input('btn_link');
+        }
+
+        if ($request->has('order')) {
+            $banner->order = $request->input('order');
+        }
+
+        if ($request->has('active')) {
+            $banner->active = $request->input('active');
+        }
+
+        $banner->save();
+
+        if ($request->ajax()) {
+            return response()->json($banner);
+        } else {
+            return back()->with('status', 'Successfully Saved!');
+        }
     }
 
     /**
