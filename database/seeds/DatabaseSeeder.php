59494e6beed0dca49ddfadb89d8758879ca7dd2e
Tsogtbayar Davaadorj <acep.world@gmail.com>
2019-09-11 16:01:34 +0800

    car stub added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call(AdministratorMenusSeeder::class);
         $this->call(ThemesTableSeeder::class);
         $this->call(PagesSeeder::class);
+        $this->call(CarPagesSeeder::class);
         $this->call(PermissionsSeeder::class);
 
         $this->call(NotificationTemplatesSeeder::class);
