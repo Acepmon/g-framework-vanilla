00d2631cfddf74245a81272ec2190c369c9dff4b
Tsogtbayar Davaadorj <acep.world@gmail.com>
2019-09-11 10:54:21 +0800

    pageheader added menus views

--- resources/views/admin/menus/create.blade.php
+++ resources/views/admin/menus/create.blade.php
@@ -8,115 +8,85 @@
 @endsection
 
 @section('pageheader')
-<div class="page-header-content">
-    <div class="page-title">
-        <h4><i class="icon-arrow-left52 position-left"></i> <span class="text-semibold">Menu</span> Create Page</h4>
-    </div>
-
-    <div class="heading-elements">
-    </div>
-</div>
-
-<div class="breadcrumb-line">
-    <ul class="breadcrumb">
-        <li><a href="{{ route('admin.menus.index') }}"><i class="icon-home2 position-left"></i> Home</a></li>
-    </ul>
-</div>
+    @include('admin.menus.includes.pageheader')
 @endsection
 
 @section('content')
-<div class="panel panel-flat">
-    <div class="panel-heading">
-        <h5 class="panel-title">Create New Menu</h5>
-        <div class="heading-elements">
-            <ul class="icons-list">
-                <li><a data-action="collapse"></a></li>
-                <li><a data-action="close"></a></li>
-            </ul>
-        </div>
-    </div>
+<div class="row">
+    <div class="col-md-6">
+        <div class="panel panel-flat">
+            <div class="panel-heading">
+                <h5 class="panel-title">Create New Menu</h5>
+                <div class="heading-elements">
+                    <ul class="icons-list">
+                        <li><a data-action="collapse"></a></li>
+                        <li><a data-action="close"></a></li>
+                    </ul>
+                </div>
+            </div>
 
-    <div class="panel-body">
-        <form class="" method="POST" action="{{ route('admin.menus.store') }}">
-            @csrf
+            <div class="panel-body">
+                <form method="POST" action="{{ route('admin.menus.store') }}">
+                    @csrf
 
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="title" class="control-label">Title</label>
-                        <input id="title" type="text" class="form-control @error('title') is-invalid @enderror" name="title" placeholder="Menu title" value="{{ old('title') }}" required autocomplete="title">
-                        @error('title')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-
-                    <div class="form-group">
-                        <label for="link" class="control-label">Link</label>
-                        <input id="link" type="text" class="form-control @error('link') is-invalid @enderror" name="link" placeholder="Menu link" value="{{ old('link') }}" required autocomplete="link">
-                        @error('link')
-                        <span class="invalid-feedback" role="alert">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="title" class="control-label">Title</label>
+                                <input id="title" type="text" class="form-control @error('title') is-invalid @enderror" name="title" placeholder="Menu title" value="{{ old('title') }}" required autocomplete="title">
+                                @error('title')
+                                <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
-                        @enderror
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label for="link" class="control-label">Link</label>
+                                <input id="link" type="text" class="form-control @error('link') is-invalid @enderror" name="link" placeholder="Menu link" value="{{ old('link') }}" required autocomplete="link">
+                                @error('link')
+                                <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label for="icon" class="control-label">Icon</label>
+                                <input id="icon" type="text" class="form-control @error('icon') is-invalid @enderror" name="icon" placeholder="Menu icon" value="{{ old('icon') }}" required autocomplete="icon">
+                                @error('icon')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label for="parent_id" class="control-label">Parent ID</label>
+                                <select class="selectbox" name="parent_id" type="text" id="parent_id">
+                                    <option value=""></option>
+                                    @foreach($menus as $data)
+                                    <option value="{{$data->id}}">{{$data->title}}</option>
+                                    @endforeach
+                                </select>
+                                @error('parent_id')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
                     </div>
 
-                    <div class="form-group">
-                        <label for="icon" class="control-label">Icon</label>
-                        <input id="icon" type="text" class="form-control @error('icon') is-invalid @enderror" name="icon" placeholder="Menu icon" value="{{ old('icon') }}" required autocomplete="icon">
-                        @error('icon')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
+                    <div class="text-right">
+                        <a class="btn btn-default" href="javascript:history.back()" type="btn btn-primary"><i class="icon-arrow-left13 position-left"></i>Back</a>
+                        <button type="submit" class="btn btn-success">Submit
+                            <i class="icon-arrow-right14 position-right"></i>
+                        </button>
                     </div>
-
-                    <div class="form-group">
-                        <label for="parent_id" class="control-label">Parent ID</label>
-                        <select class="selectbox" name="parent_id" type="text" id="parent_id">
-                            <option value=""></option>
-                            @foreach($menus as $data)
-                            <option value="{{$data->id}}">{{$data->title}}</option>
-                            @endforeach
-                        </select>
-                        @error('parent_id')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="order" class="control-label">Order</label>
-                        <input id="order" type="text" class="form-control @error('order') is-invalid @enderror" name="order" placeholder="Menu order" value="{{ old('order') }}" required autocomplete="order">
-                        @error('order')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-
-                    <div class="form-group">
-                        <label for="sublevel" class="control-label">Sublevel</label>
-                        <input id="sublevel" type="text" class="form-control @error('sublevel') is-invalid @enderror" name="sublevel" placeholder="Menu sublevel" value="{{ old('sublevel') }}" required autocomplete="sublevel">
-                        @error('sublevel')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                </div>
+                </form>
             </div>
-
-            <div class="text-right">
-                <a class="btn btn-default" href="javascript:history.back()" type="btn btn-primary"><i class="icon-arrow-left13 position-left"></i>Back</a>
-                <button type="submit" class="btn btn-success">Create menu
-                    <i class="icon-arrow-right14 position-right"></i>
-                </button>
-            </div>
-        </form>
+        </div>
     </div>
 </div>
 @endsection
---
+++ resources/views/admin/menus/includes/pageheader.blade.php
@@ -0,0 +1,13 @@
+<div class="page-header-content">
+    <div class="page-title">
+        <h4><i class="icon-arrow-left52 position-left"></i> <span class="text-semibold">Menus</span> - @yield('title')</h4>
+    </div>
+</div>
+
+<div class="breadcrumb-line">
+    <ul class="breadcrumb">
+        <li><a href="{{ route('admin.dashboard') }}"><i class="icon-home2 position-left"></i> Home</a></li>
+        <li><a href="{{ route('admin.menus.index') }}">Menus</a></li>
+        <li class="active">Detail</li>
+    </ul>
+</div>
