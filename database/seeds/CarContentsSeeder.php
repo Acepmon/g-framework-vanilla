dca57280c89039b75f0dea43405c20f3948997e2
batkhuyag <batkhuyag.batjargal@gmail.com>
2019-09-13 15:28:31 +0800

    carcontentseeder

--- database/seeds/CarContentsSeeder.php
+++ database/seeds/CarContentsSeeder.php
@@ -17,21 +17,21 @@ class CarContentsSeeder extends Seeder
             	$content->metas()->saveMany([ 
 				    new App\ContentMeta(['key' => 'plateNumber', 'value' => '0035UNA']),
 				    new App\ContentMeta(['key' => 'cabinNumber', 'value' => 'VF3 3CRFNC 12345678']),
-				    new App\ContentMeta(['key' => 'countryName', 'value' => 'Korea']),
+				    new App\ContentMeta(['key' => 'countryName', 'value' => 'Japan']),
 				    new App\ContentMeta(['key' => 'markName', 'value' => 'toyota']),
-				    new App\ContentMeta(['key' => 'modelName', 'value' => 'prius']),
-				    new App\ContentMeta(['key' => 'type', 'value' => 'Sedan']),
+				    new App\ContentMeta(['key' => 'modelName', 'value' => 'starix']),
+				    new App\ContentMeta(['key' => 'type', 'value' => 'van']),
 				    new App\ContentMeta(['key' => 'className', 'value' => 'luxury']),
 				    new App\ContentMeta(['key' => 'manCount', 'value' => '4']),
-				    new App\ContentMeta(['key' => 'weight', 'value' => '1200kg']),
-				    new App\ContentMeta(['key' => 'mass', 'value' => '1200kg']),
+				    new App\ContentMeta(['key' => 'weight', 'value' => '1600kg']),
+				    new App\ContentMeta(['key' => 'mass', 'value' => '1600kg']),
 				    new App\ContentMeta(['key' => 'fuelType', 'value' => 'gas']),
 				    new App\ContentMeta(['key' => 'width', 'value' => '160cm']),
-				    new App\ContentMeta(['key' => 'height', 'value' => '120cm']),
-				    new App\ContentMeta(['key' => 'capacity', 'value' => '1500cc']),
+				    new App\ContentMeta(['key' => 'height', 'value' => '180cm']),
+				    new App\ContentMeta(['key' => 'capacity', 'value' => '1800cc']),
 				    new App\ContentMeta(['key' => 'motorNumber', 'value' => '2H2tXA598WDY987665']),
 				    new App\ContentMeta(['key' => 'colorName', 'value' => 'black']),
-				    new App\ContentMeta(['key' => 'axleCount', 'value' => '2']),
+				    new App\ContentMeta(['key' => 'axleCount', 'value' => '4']),
 				    new App\ContentMeta(['key' => 'certificateNumber', 'value' => '2H2tXA598WDY987665']),
 				    new App\ContentMeta(['key' => 'importDate', 'value' => '2006']),
 				    new App\ContentMeta(['key' => 'intent', 'value' => 'use']),
@@ -39,7 +39,7 @@ class CarContentsSeeder extends Seeder
 				    new App\ContentMeta(['key' => 'archiveDate', 'value' => '2008']),
 				    new App\ContentMeta(['key' => 'buildYear', 'value' => '2003']),
 				    new App\ContentMeta(['key' => 'archiveFirstNumber', 'value' => 'A598WDY987']),
-				    new App\ContentMeta(['key' => 'wheelPosition', 'value' => 'left']),
+				    new App\ContentMeta(['key' => 'wheelPosition', 'value' => 'right']),
 				    new App\ContentMeta(['key' => 'length', 'value' => '4m']),
 				    new App\ContentMeta(['key' => 'archiveNumber', 'value' => 'A598WDY987']),
 				    new App\ContentMeta(['key' => 'title', 'value' => 'Selling car']),
@@ -61,7 +61,6 @@ class CarContentsSeeder extends Seeder
 				    new App\ContentMeta(['key' => 'maxBid', 'value' => '14000000']),
 				    new App\ContentMeta(['key' => 'startsAt', 'value' => '2019-09-15']),
 				    new App\ContentMeta(['key' => 'endsAt', 'value' => '2019-09-18'])
-
 				]);
             } else {
             	$content->metas()->save(factory(App\ContentMeta::class)->make());
