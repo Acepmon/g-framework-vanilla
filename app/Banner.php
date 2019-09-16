a80dadfb64e4b2ff75fc2d8a64ccb6a0db099182
Tsogtbayar Davaadorj <acep.world@gmail.com>
2019-09-16 19:13:19 +0800

    banner update - backend complete

--- app/Banner.php
+++ app/Banner.php
@@ -6,8 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Banner extends Model
 {
-    public const MOBILE_WIDTH = 640;
-    public const MOBILE_HEIGHT = 360;
-    public const WEB_WIDTH = 1920;
-    public const WEB_HEIGHT = 1080;
+    const STATUS_ACTIVE = 'active';
+    const STATUS_DRAFT = 'draft';
+
+    const STATUS_ARRAY = [
+        self::STATUS_ACTIVE,
+        self::STATUS_DRAFT
+    ];
+
+    public function location()
+    {
+        return $this->hasOne('App\BannerLocation');
+    }
 }
---
+++ database/seeds/CarBannersTableSeeder.php
@@ -0,0 +1,81 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\BannerLocation;
+
+class CarBannersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $url = "http://lorempixel.com/";
+
+        // 1. Home Main Slider (650x650)
+        factory(App\Banner::class, 3)->create([
+            "location_id" => 1
+        ])->each(function ($banner) use ($url) {
+            $banner->banner = $url . BannerLocation::find(1)->width . "/" . BannerLocation::find(1)->height . "/transport/?=" . rand(1, 5000);
+            $banner->save();
+        });
+
+        // 2. Login Main Slider (650x650)
+        factory(App\Banner::class, 3)->create([
+            "location_id" => 2
+        ])->each(function ($banner) use ($url) {
+            $banner->banner = $url . BannerLocation::find(2)->width . "/" . BannerLocation::find(2)->height . "/transport/?=" . rand(1, 5000);
+            $banner->save();
+        });
+
+        // 3. Finance Main Slider (650x650)
+        factory(App\Banner::class, 3)->create([
+            "location_id" => 3
+        ])->each(function ($banner) use ($url) {
+            $banner->banner = $url . BannerLocation::find(3)->width . "/" . BannerLocation::find(3)->height . "/transport/?=" . rand(1, 5000);
+            $banner->save();
+        });
+
+        // 4. Right Sidebar Sticky (120x160)
+        factory(App\Banner::class, 2)->create([
+            "location_id" => 4
+        ])->each(function ($banner) use ($url) {
+            $banner->banner = $url . BannerLocation::find(4)->width . "/" . BannerLocation::find(4)->height . "/transport/?=" . rand(1, 5000);
+            $banner->save();
+        });
+
+        // 5. Mid Content Banner (1170x160)
+        factory(App\Banner::class, 3)->create([
+            "location_id" => 5
+        ])->each(function ($banner) use ($url) {
+            $banner->banner = $url . BannerLocation::find(5)->width . "/" . BannerLocation::find(5)->height . "/transport/?=" . rand(1, 5000);
+            $banner->save();
+        });
+
+        // 6. Mobile Home Main Slider (640x360)
+        factory(App\Banner::class, 3)->create([
+            "location_id" => 6
+        ])->each(function ($banner) use ($url) {
+            $banner->banner = $url . BannerLocation::find(6)->width . "/" . BannerLocation::find(6)->height . "/transport/?=" . rand(1, 5000);
+            $banner->save();
+        });
+
+        // 7. Mobile Mid Content Banner (640x360)
+        factory(App\Banner::class, 3)->create([
+            "location_id" => 7
+        ])->each(function ($banner) use ($url) {
+            $banner->banner = $url . BannerLocation::find(7)->width . "/" . BannerLocation::find(7)->height . "/transport/?=" . rand(1, 5000);
+            $banner->save();
+        });
+
+        // 8. Mobile Finance Main Slider (640x360)
+        factory(App\Banner::class, 3)->create([
+            "location_id" => 8
+        ])->each(function ($banner) use ($url) {
+            $banner->banner = $url . BannerLocation::find(8)->width . "/" . BannerLocation::find(8)->height . "/transport/?=" . rand(1, 5000);
+            $banner->save();
+        });
+    }
+}
