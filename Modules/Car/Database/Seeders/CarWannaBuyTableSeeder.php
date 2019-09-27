45bdc9656c06538ae7e6606d35ac50ae9cd07f55
Tsogtbayar Davaadorj <acep.world@gmail.com>
2019-09-27 12:01:12 +0800

    interested cars seed & api added

---
+++ Modules/Car/Database/Seeders/CarWannaBuyTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Modules\Car\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+use App\User;
+use App\UserMeta;
+
+class CarWannaBuyTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+
+    }
+}
