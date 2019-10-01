69fdb6cab9f9d1c9f03ab5c378701e1e7b16c123
Tsogtbayar Davaadorj <acep.world@gmail.com>
2019-10-02 03:04:15 +0800

    make manager command created

---
+++ app/Entities/Manager.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Support\Facades\DB;
+
+class Manager
+{
+    protected static function handle($callback) {
+        try {
+            DB::beginTransaction();
+
+            if (is_callable($callback)) {
+                return $callback();
+            }
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+}
