67b9a009e36d601d09b197ff47bb2c9e453560ac
hatnaa <hatnaa.k@gmail.com>
2019-10-02 11:46:11 +0800

    content manager

--- app/Entities/ContentManager.php
+++ app/Entities/ContentManager.php
@@ -16,10 +16,11 @@ class ContentManager extends Manager
      * Does not care about if key exists or not
      * Can support array as value
      */
-    public static function addMeta($content_id, $key, $value) {
+    public static function addMeta($content_id, $key, $value)
+    {
         if ($value) {
             if (is_array($value)) {
-                foreach($value as $v) {
+                foreach ($value as $v) {
                     self::addMeta($content_id, $key, $v);
                 }
             } else {
@@ -38,12 +39,13 @@ class ContentManager extends Manager
      * Updates all metas matching key, value pair
      * @return meta if successful, error message if fail
      */
-    public static function updateMeta($content_id, $key, $value, $newValue) {
+    public static function updateMeta($content_id, $key, $value, $newValue)
+    {
         if ($newValue) {
             $metas = ContentMeta::where([['content_id', $content_id], ['key', $key], ['value', $value]])->get();
             $result = null;
-            foreach($metas as $meta) {
-                if($meta) {
+            foreach ($metas as $meta) {
+                if ($meta) {
                     if (is_array($newValue)) {
                         $meta->delete();
                         self::addMeta($content_id, $key, $newValue);
@@ -54,8 +56,10 @@ class ContentManager extends Manager
                     $result = $meta;
                 }
             }
-            if ($result)
+            if ($result) {
                 return $result;
+            }
+
         }
         return ["error" => "No such meta found"];
     }
@@ -64,10 +68,11 @@ class ContentManager extends Manager
      * Deletas ALL content meta
      * @return meta if successful, error message if fail
      */
-    public static function deleteMeta($content_id, $key, $value) {
+    public static function deleteMeta($content_id, $key, $value)
+    {
         if ($value) {
             if (is_array($value)) {
-                foreach($value as $v) {
+                foreach ($value as $v) {
                     self::deleteMeta($content_id, $key, $v);
                 }
             } else {
@@ -83,20 +88,21 @@ class ContentManager extends Manager
      * Replace all values
      * @return meta if successful, error message if fail
      */
-    public static function syncMetas($content_id, $metas) {
+    public static function syncMetas($content_id, $metas)
+    {
         try {
             DB::beginTransaction();
-            foreach($metas as $key=>$value) {
+            foreach ($metas as $key => $value) {
                 $oldMetas = ContentMeta::where([['content_id', $content_id], ['key', $key]]);
                 if (is_array($value)) {
-                    foreach($oldMetas->get() as $meta) {
+                    foreach ($oldMetas->get() as $meta) {
                         if (in_array($meta->value, $value)) {
                             $value = array_diff($value, [$meta->value]);
                         } else {
                             self::deleteMeta($content_id, $key, $meta->value);
                         }
                     }
-                    foreach($value as $v) {
+                    foreach ($value as $v) {
                         self::addMeta($content_id, $key, $v);
                     }
                 } else {
@@ -117,10 +123,11 @@ class ContentManager extends Manager
     /**
      * Mass add metas
      */
-    public static function attachMetas($content_id, $metas) {
+    public static function attachMetas($content_id, $metas)
+    {
         try {
             DB::beginTransaction();
-            foreach($metas as $key=>$value) {
+            foreach ($metas as $key => $value) {
                 self::addMeta($content_id, $key, $value);
             }
             DB::commit();
@@ -133,10 +140,11 @@ class ContentManager extends Manager
     /**
      * Mass delete metas
      */
-    public static function detachMetas($content_id, $metas) {
+    public static function detachMetas($content_id, $metas)
+    {
         try {
             DB::beginTransaction();
-            foreach($metas as $key=>$value) {
+            foreach ($metas as $key => $value) {
                 if (is_array($value)) {
                     self::deleteMeta($content_id, $key, $value);
 
@@ -155,12 +163,8 @@ class ContentManager extends Manager
      * Filter & Serialize mixed request of Content and ContentMetas to Content Object with ContentMeta subarray
      * @return Paginated Content
      */
-<<<<<<< HEAD
     public static function serializeRequest(Request $request)
     {
-=======
-    public static function serializeRequest(Request $request) {
->>>>>>> 2a63581710a9622b5be413fdfe0ed9e2d7518f4b
         $type = $request->input('type', Content::TYPE_POST);
         $status = $request->input('status', Content::STATUS_PUBLISHED);
         $visibility = $request->input('visibility', Content::VISIBILITY_PUBLIC);
@@ -191,14 +195,9 @@ class ContentManager extends Manager
      * Used to discern content metas from attributes of Content Model.
      * @return ContentMeta array, consisting of key, value pairs
      */
-<<<<<<< HEAD
     public static function discernMetasFromRequest($input)
     {
-        $inputExcept = ['type', 'status', 'visibility', 'limit', 'page', 'author_id'];
-=======
-    public static function discernMetasFromRequest($input) {
         $inputExcept = ['title', 'slug', 'content', 'type', 'status', 'visibility', 'limit', 'page', 'author_id'];
->>>>>>> 2a63581710a9622b5be413fdfe0ed9e2d7518f4b
         $metaInputs = array_filter($input, function ($key) use ($inputExcept) {
             return !in_array($key, $inputExcept);
         }, ARRAY_FILTER_USE_KEY);
@@ -210,12 +209,8 @@ class ContentManager extends Manager
      * Can take additional parameters to merge
      * @return content array
      */
-<<<<<<< HEAD
     public static function contentToArray(Content $content, $additional = null)
     {
-=======
-    public static function contentToArray(Content $content, $additional = null) {
->>>>>>> 2a63581710a9622b5be413fdfe0ed9e2d7518f4b
         $result = [
             "id" => $content->id,
             "title" => $content->title,
