bee516145cfa93cd7061183e3f2d31362d4a7aa9
Tsogtbayar <dtsogtbayar123@gmail.com>
2019-10-05 14:45:49 +0800

    optional configuration for car module seeder

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,60 +1,76 @@
-@extends('themes.car-web.layouts.default')
-@section('load')
+@extends('themes.limitless.layouts.login')
 
-@endsection
-@section('title')
+@section('title', __('Reset Password'))
+
+@section('load')
 
 @endsection
 
 @section('content')
-    <!-- Masthead -->
-    <header class="masthead text-center">
-        <div class="container">
-            <div class="w-50 d-inline-block">
-                <div class="card masthead-search shadow-lg-3d">
-                    <div class="card-header px-5 pt-5 pb-3 mb-3">
-                        <h1>Recover password</h1>
-                    </div>
-                    <div class="col-12 p-5">
-                        <form method="POST" action="{{ route('password.update') }}">
-                            @csrf
-                            <input type="hidden" name="token" value="{{ $token }}">
-                            <div class="form-group mb-3 text-left form-group-feedback form-group-feedback-left">
-                                <span class="font-weight-bold">Email</span>
+<!-- Content area -->
+<div class="content d-flex justify-content-left align-items-center">
 
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+    <!-- Password recovery form -->
+    <form method="POST" action="{{ route('password.update') }}">
+        @csrf
 
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                            <div class="form-group text-left">
-                                <span class="font-weight-bold">Password</span>
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                            <div class="form-group text-left">
-                                <span class="font-weight-bold">Password confirm</span>
-
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                            <button type="submit" class="btn col-12 btn-primary btn-lg btn-round shadow mt-4">Save</button>
-                        </form>
-                        {{--<a class="" href="#">Forget password</a>--}}
-                        {{--<div class="col-12 mt-5">--}}
-                        {{--<button type="button" class="btn bg-secondary col-9 btn-default btn-lg btn-round mt-4">Sign up</button>--}}
-                        {{--</div>--}}
-
-                    </div>
+        <div class="card mb-0 wmin-sm-300">
+            <div class="card-body">
+                <div class="text-center mb-3">
+                    <i class="icon-spinner11 icon-2x text-warning border-warning border-3 rounded-round p-3 mb-3 mt-1"></i>
+                    <h5 class="mb-0">{{ __('Reset Password') }}</h5>
+                    <span class="d-block text-muted">Please choose a new password</span>
                 </div>
+
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+                <input type="hidden" name="token" value="{{ $token }}">
+
+                <div class="form-group">
+                    <label for="email" class="col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            {{ $message }}
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <label for="password" class="col-form-label text-md-right">{{ __('Password') }}</label>
+
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            {{ $message }}
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <label for="password-confirm" class="col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">
+                        {{ __('Reset Password') }}
+                    </button>
+                </div>
+
             </div>
         </div>
-    </header>
+    </form>
+    <!-- /password recovery form -->
+
+</div>
+<!-- /content area -->
 @endsection
