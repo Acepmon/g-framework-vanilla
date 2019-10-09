dd2cfbee19efc83c98514f9a7283e6c5744356b3
hatnaa <hatnaa.k@gmail.com>
2019-10-09 09:25:16 +0800

    content

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,80 +1,193 @@
-@extends('themes.limitless.layouts.login')
-
-@section('title', 'Login')
-
+@extends('themes.car-web.layouts.default')
 @section('load')
+
+@endsection
+@section('title')
 
 @endsection
 
 @section('content')
-<div class="content d-flex justify-content-left align-items-center">
-    <form class="login-form" method="POST" action="{{ route('login') }}">
-        @csrf
-        <div class="card mb-0">
-            <div class="card-body">
-                <div class="text-center mb-3">
-                    <i class="icon-reading icon-2x text-slate-300 border-slate-300 border-3 rounded-round p-3 mb-3 mt-1"></i>
-                    <h5 class="mb-0">Login to your account</h5>
-                    <span class="d-block text-muted">Enter your credentials below</span>
-                </div>
-                <div class="form-group form-group-feedback form-group-feedback-left">
-                    <input id="username" type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Username" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
-                    @error('username')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-                    <div class="form-control-feedback">
-                        <i class="icon-user text-muted"></i>
+    <!-- Masthead -->
+    <header class="masthead text-center">
+        <div class="container">
+                <div class="row">
+                    <div class="col-md-7">
+                        <div class="hero-slider owl-carousel owl-theme">
+
+                            @banners([{"field":"location_id", "key":1}])
+                            @php
+                                $bannerList=json_decode($banners);
+                            @endphp
+                            @if(count($bannerList) > 0)
+                                @foreach($bannerList as $bnr)
+                                    <div class="slider-item">
+                                        <div class="slider-img animated slideInLeft" style="bottom: 0; left: -50px;">
+                                            <a href="{{$bnr->link}}" target="_blank">
+                                                <img src="{{$bnr->banner}}" alt="">
+                                            </a>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            @else
+                                <div class="slider-item">
+                                    <div class="slider-text animated slideInLeft" style="top: 0; left: 100px">
+                                        <h1>Зээлийн хүү</h1>
+                                    </div>
+                                    <div class="slider-text animated slideInLeft slider-highlight"
+                                         style="top: 50px; left: 50px">3%
+                                    </div>
+                                    <div class="slider-text animated slideInLeft" style="top: 200px; left: 200px">
+                                        <h1>Жилийн</h1>
+                                    </div>
+                                    <div class="slider-img animated slideInLeft" style="bottom: 0; left: -50px;">
+                                        <img src="{{asset('car-web/img/slider-1.png')}}" alt="">
+                                    </div>
+                                </div>
+                                <div class="slider-item">
+                                    <div class="slider-text" style="top: 5%; left: 90px">
+                                        <h1>Авто машины</h1>
+                                    </div>
+                                    <div class="slider-text" style="top: 18%; left: 100px; ">
+                                        <h1 style="font-size: 2rem;">Дуудлага худалдаа</h1>
+                                    </div>
+                                    <div class="slider-img" style="top: 50%; left: 50px;">
+                                        <img src="{{asset('car-web/img/slider-2.png')}}" alt="" class="img-fluid">
+                                    </div>
+                                    <div class="slider-img" style="top: 25%; left: 0; z-index:-1;">
+                                        <img src="{{asset('car-web/img/auction.png')}}" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="col-md-5" style="z-index: 1">
+                        <div class="card masthead-search shadow-lg-3d">
+                            <div class="">
+                                <div class="card-header px-5 pt-5 pb-3 mb-3">
+                                    <h1>Car dealer login</h1>
+                                </div>
+                                <div class="col-12 p-5">
+                                    <form method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                        <div class="form-group mb-3 text-left form-group-feedback form-group-feedback-left">
+                                            <span class="font-weight-bold">Username</span>
+
+                                            <input class="form-control mt-2 @error('name') is-invalid @enderror" id="username" type="text" placeholder="Username" name="username" value="{{ old('username', env('APP_ENV') == 'development' ? 'admin' : '') }}" required autocomplete="username" autofocus>
+                                            @error('username')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group text-left">
+                                            <span class="font-weight-bold">Password</span>
+                                            <input id="password" type="password" class="form-control mt-2 @error('password') is-invalid @enderror" placeholder="Password" name="password" value="{{ env('APP_ENV') == 'development' ? 'admin' : '' }}" required autocomplete="current-password">
+                                            @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+                                        <button type="submit" class="btn col-12 btn-danger btn-lg btn-round shadow mt-4 mb-3">Login</button>
+                                    </form>
+                                    <a href="#" data-toggle="modal" data-target="#myModal">Forget password</a>
+                                    <div class="col-12 mt-5">
+                                        <button type="button" class="btn bg-secondary col-9 btn-default btn-lg btn-round mt-4">Sign up</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
+            </div>
+    </header>
 
-                <div class="form-group form-group-feedback form-group-feedback-left">
-                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" name="password" required autocomplete="current-password">
-                    @error('password')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-                    <div class="form-control-feedback">
-                        <i class="icon-lock2 text-muted"></i>
-                    </div>
+    <!-- The Modal -->
+    <div class="modal fade" id="myModal">
+        <div class="modal-dialog  modal-dialog-centered">
+            <div class="modal-content">
+
+                <!-- Modal Header -->
+                <div class="modal-header text-center">
+                    <h4 class="modal-title">Forget password</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
                 </div>
 
-                <div class="form-group d-flex align-items-center">
-                    <div class="form-check mb-0">
-                        <label class="form-check-label">
-                            <input type="checkbox" name="remember" id="remember" class="form-input-styled" {{ old('remember') ? 'checked' : '' }} data-fouc>
-                            Remember Me
-                        </label>
-                    </div>
+                <!-- Modal body -->
+                <div class="modal-body mt-4 pr-lg-5 pl-lg-5 text-center">
+                    <form method="POST">
+                        @csrf
+                        <div class="form-group mb-3 text-left form-group-feedback form-group-feedback-left">
+                            <span class="font-weight-bold">Email address</span>
 
-                    <a href="{{ route('password.request') }}" class="ml-auto">Forgot password?</a>
+                            <input class="form-control mt-2 @error('email') is-invalid @enderror" id="email" type="text" placeholder="Email" name="email" value="{{ old('username', env('APP_ENV') == 'development' ? 'admin' : '') }}" required autocomplete="email" autofocus>
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                            @enderror
+                        </div>
+                        <button type="submit" class="btn btn-submit col-12 btn-danger btn-lg btn-round shadow mt-4 mb-4">Recover password</button>
+                        <a href="#" class="text-dark" data-dismiss="modal">Login</a>
+                    </form>
                 </div>
 
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-round btn-secondary mt-5 col-md-6 mb-5" data-dismiss="modal">sign up</button>
                 </div>
 
-                <div class="form-group text-center text-muted content-divider">
-                    <span class="px-2">or sign in with</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="mailSuccess">
+        <div class="modal-dialog  modal-dialog-centered">
+            <div class="modal-content">
+
+                <!-- Modal Header -->
+                <div class="modal-header text-center">
+                    <h4 class="modal-title">Forget password</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
                 </div>
 
-                <div class="form-group text-center">
-                    <a href="{{ route('login.provider', 'facebook') }}" class="btn btn-outline bg-indigo border-indigo text-indigo btn-icon rounded-round border-2"><i class="icon-facebook"></i></a>
-                    <a href="{{ route('login.provider', 'google') }}" class="btn btn-outline bg-pink-300 border-pink-300 text-pink-300 btn-icon rounded-round border-2 ml-2"><i class="icon-google"></i></a>
-                </div>
-
-                <div class="form-group text-center text-muted content-divider">
-                    <span class="px-2">Don't have an account?</span>
-                </div>
-
-                <div class="form-group">
-                    <a href="{{ route('register') }}" class="btn btn-light btn-block">Register</a>
+                <!-- Modal body -->
+                <div class="modal-body mt-4 pr-lg-5 pl-lg-5 text-center">
+                   Mail has been sent!
                 </div>
             </div>
         </div>
-    </form>
-</div>
+    </div>
 
+@endsection
+
+@section('script')
+    <script type="text/javascript">
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+        $(".btn-submit").click(function(e){
+            e.preventDefault();
+
+            var email = $("input[name=email]").val();
+
+            $.ajax({
+                type:'POST',
+                url: "{{ route('password.email') }}",
+                data:{email:email},
+                success:function(data){
+                    $('#myModal').modal('hide');
+                    $('#mailSuccess').modal('show');
+
+                }
+
+            });
+
+
+
+        });
+
+    </script>
 @endsection
