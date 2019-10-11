becba9f3d4f0ed23e5c41d0f95c1b67afbf35a92
munkhuchral <uchral_1003@yahoo.com>
2019-10-11 17:17:37 +0800

    wanna buy zasvar bolon interested car

--- Modules/Car/Database/Seeders/CarPagesTableSeeder.php
+++ Modules/Car/Database/Seeders/CarPagesTableSeeder.php
@@ -402,6 +402,40 @@ class CarPagesTableSeeder extends Seeder
 
         file_put_contents(base_path($file_path), $file_content);
 
+        // --- car my page interested car registration alert
+
+        $time = time();
+        $rootPath = config('content.pages.rootPath');
+
+        $content = new Content;
+        $content->title = 'Interested car registration alert';
+        $content->slug = 'interested-car-registration-alert';
+        $content->type = Content::TYPE_PAGE;
+        $content->status = Content::STATUS_PUBLISHED;
+        $content->visibility = Content::VISIBILITY_PUBLIC;
+        $content->author_id = 1;
+        $content->save();
+
+        $value = new \stdClass;
+        $value->datetime = $time;
+        $value->filename_changed = true;
+        $value->before = $content;
+        $value->after = $content;
+        $value->user = User::find(1);
+
+        $content_meta = new ContentMeta();
+        $content_meta->content_id = $content->id;
+        $content_meta->key = 'initial';
+        $content_meta->value = json_encode($value);
+        $content_meta->save();
+
+        $file_content = file_get_contents(resource_path('stubs/carInterestedRegistrationAlert.stub'));
+        $file_name = $rootPath . DIRECTORY_SEPARATOR . 'interested-car-registration-alert' . Content::NAMING_CONVENTION . $content->status . Content::NAMING_CONVENTION . $time;
+        $file_ext = 'blade.php';
+        $file_path = $file_name . '.' . $file_ext;
+
+        file_put_contents(base_path($file_path), $file_content);
+
         // --- car my page sell request on sell ---
 
         $time = time();
