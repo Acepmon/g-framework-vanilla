89ee6cd7a0f5ba88d93e9e0be32713b715fcadc2
Tsogtbayar Davaadorj <acep.world@gmail.com>
2019-10-16 17:54:16 +0800

    car options index & show page compltee

--- Modules/Car/Resources/views/admin/car/options/index.blade.php
+++ Modules/Car/Resources/views/admin/car/options/index.blade.php
@@ -15,7 +15,23 @@
 @endsection
 
 @section('content')
+<div class="d-md-flex align-items-md-start">
 
+    @include('car::admin.car.options.includes.sidebar')
+
+    <!-- Right content -->
+    <div class="flex-fill overflow-auto">
+
+        <div class="card">
+            <div class="card-header bg-transparent header-elements-inline">
+                <h6 class="card-title">My Inbox (single line)</h6>
+            </div>
+        </div>
+
+    </div>
+    <!-- /right content -->
+
+</div>
 @endsection
 
 @section('script')
