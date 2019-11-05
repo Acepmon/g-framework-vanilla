aecddc598152bc43c312cf6468da14eab968f6bb
vis5 <themonmail@gmail.com>
2019-11-05 17:07:26 +0800

    added car edit

--- Modules/Car/Database/Seeders/CarSellPagesTableSeeder.php
+++ Modules/Car/Database/Seeders/CarSellPagesTableSeeder.php
@@ -207,6 +207,38 @@ class CarSellPagesTableSeeder extends Seeder
             $file_path = $file_name . '.' . $file_ext;
 
             file_put_contents(base_path($file_path), $file_content);
+
+
+            // --- car sell edit page ---
+            $content = new Content;
+            $content->title = 'Sell Car Edit Page';
+            $content->slug = 'edit';
+            $content->type = Content::TYPE_PAGE;
+            $content->status = Content::STATUS_PUBLISHED;
+            $content->visibility = Content::VISIBILITY_AUTH;
+            $content->author_id = 1;
+            $content->save();
+
+            $value = new \stdClass;
+            $value->datetime = $time;
+            $value->filename_changed = true;
+            $value->before = $content;
+            $value->after = $content;
+            $value->user = User::find(1);
+
+            $content_meta = new ContentMeta();
+            $content_meta->content_id = $content->id;
+            $content_meta->key = 'initial';
+            $content_meta->value = json_encode($value);
+            $content_meta->save();
+
+            $file_content = file_get_contents(resource_path('stubs/carSellEdit.stub'));
+            $file_name = $rootPath . DIRECTORY_SEPARATOR . 'edit' . Content::NAMING_CONVENTION . $content->status . Content::NAMING_CONVENTION . $time;
+            $file_ext = 'blade.php';
+            $file_path = $file_name . '.' . $file_ext;
+
+            file_put_contents(base_path($file_path), $file_content);
+
         } catch (\Exception $e) {
             throw $e;
         }
