2f53f7e3483d412afd580f15de375cc8b6da291f
Tuvshuu1211 <tuvshuu1211@gmail.com>
2019-11-06 15:39:45 +0800

    fixed right bar update

--- resources/views/themes/car-web/layouts/default.blade.php
+++ resources/views/themes/car-web/layouts/default.blade.php
@@ -31,6 +31,7 @@
 
     <script src="{{ asset('car-web/vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('car-web/js/helpers.js') }}"></script>
+    <script src="{{ asset('car-web/vendor/lottie-web/player/lottie.min.js') }}"></script>
     @yield('load')
 
     @stack('styles')
@@ -49,7 +50,6 @@
     <script src="{{ asset('car-web/vendor/owl.carousel/owl.carousel.min.js') }}"></script>
     <script src="{{ asset('car-web/vendor/owl.carousel.thumbs/owl.carousel2.thumbs.min.js') }}"></script>
     <script src="{{ asset('car-web/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-    <script src="{{ asset('car-web/vendor/lottie-web/player/lottie.min.js') }}"></script>
     <script src="{{ asset('car-web/js/script.js') }}"></script>
     <script src="{{ asset('inputmask/jquery.inputmask.min.js') }}"></script>
     <script>
