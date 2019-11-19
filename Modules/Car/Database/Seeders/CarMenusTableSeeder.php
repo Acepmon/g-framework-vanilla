a888f982a7c538ecc3481bfc380ebc102332b6e2
vis5 <themonmail@gmail.com>
2019-11-19 17:22:43 +0800

    fixed seeder

--- Modules/Car/Database/Seeders/CarMenusTableSeeder.php
+++ Modules/Car/Database/Seeders/CarMenusTableSeeder.php
@@ -35,20 +35,20 @@ class CarMenusTableSeeder extends Seeder
             ['Wishlist', '/admin/modules/car/wishlist', 'icon-star-empty3', 'Car'],
         ];
         $carMainMenus = ['Car Main', '', '', 'Car', [
-            ['Home', '/', '', 'Car'],
-            ['Buy', '/buy', '', 'Car'],
-            ['Sell', '/sell', '', 'Car'],
-            ['Finance', '/finance', '', 'Car'],
-            ['Auction', '/auction', '', 'Car'],
-            ['Wish List', '/wishlist', '', 'Car']
+            ['Нүүр', '/', '', 'Car'],
+            ['Авна', '/buy', '', 'Car'],
+            ['Зарна', '/sell', '', 'Car'],
+            ['Санхүүжилт', '/finance', '', 'Car'],
+            ['Аугцион', '/auction', '', 'Car'],
+            ['Авъя', '/wishlist', '', 'Car']
         ]];
         $carTopbarMenus = ['Car Topbar', '', '', 'Car', [
             ['Бидний тухай', '/about-introduction', '', 'Car'],
             ['+Хүсэлт оруулах', '/wishlist', '', 'Car']
         ]];
         $carFooterMenus = ['Car Footer', '', '', 'Car', [
-            ['About Us', '/about-introduction', '', 'Car'],
-            ['Write wanna buy', '/wishlist', '', 'Car']
+            ['Бидний тухай', '/about-introduction', '', 'Car'],
+            ['Хүслийн жагсаалт', '/wishlist', '', 'Car']
         ]];
         $carProfileDropdownMenus = ['Car Profile Dropdown', '', '', 'Car', [
             ['Сонирхож буй машинууд', '/interested-car', '', 'Car'],
