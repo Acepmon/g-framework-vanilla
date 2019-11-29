f813c65a9e9120778cffcc831e8fdfb8ca7293b9
vis5 <themonmail@gmail.com>
2019-11-29 12:21:52 +0800

    fix seeder bug

--- Modules/System/Database/Seeders/UserGroupTableSeeder.php
+++ Modules/System/Database/Seeders/UserGroupTableSeeder.php
@@ -31,8 +31,8 @@ class UserGroupTableSeeder extends Seeder
             User::where('username', 'content')->first()->groups()->attach(Group::where('title', 'Content Operator')->get());
 
             // Custom User
-            User::where('username', 'Damoa Capital')->first()->groups()->attach(Group::where('title', 'Operator')->get());
-            User::where('username', 'Damoa Capital')->first()->groups()->attach(Group::where('title', 'System Operator')->get());
+            User::where('username', 'damoa')->first()->groups()->attach(Group::where('title', 'Operator')->get());
+            User::where('username', 'damoa')->first()->groups()->attach(Group::where('title', 'System Operator')->get());
         }
     }
 }
